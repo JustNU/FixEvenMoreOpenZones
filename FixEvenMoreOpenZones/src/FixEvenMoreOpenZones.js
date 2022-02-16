1320a00751cb422b02bc72018fe50eb5628f8d09
JustNU <ivancool108@gmail.com>
2022-02-16 12:42:41 +0300

    fix the config issue

--- FixEvenMoreOpenZones/src/FixEvenMoreOpenZones.js
+++ FixEvenMoreOpenZones/src/FixEvenMoreOpenZones.js
@@ -10,6 +10,13 @@ class FixEvenMoreOpenZones
 			//Logger.log(DatabaseServer.tables.locations[location].base.OpenZones)
 			DatabaseServer.tables.locations[location].base.OpenZones = config.zones[location]
 			//Logger.log(DatabaseServer.tables.locations[location].base.OpenZones)
+			
+			// el debugio
+			/*
+			for (const wave in DatabaseServer.tables.locations[location].base.waves) {
+				DatabaseServer.tables.locations[location].base.waves[wave].SpawnPoints = "";
+			}
+			*/
 		}
 	}
 }
